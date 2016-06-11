15f475d4d2656a85a8c747f59f403890444605e0
Csniadach <cyril.sniadach@viacesi.fr>
2016-06-11 18:48:42 +0200

    All tests running and passing
    All the tests of the project are green

---
+++ view/src/test/java/view/ViewFrameTest.java
@@ -0,0 +1,50 @@
+package view;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ViewFrameTest {
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSetController() throws Exception {
+
+    }
+
+    @Test
+    public void testGetModel() throws Exception {
+
+    }
+
+    @Test
+    public void testPrintMessage() throws Exception {
+
+    }
+
+    @Test
+    public void testKeyTyped() throws Exception {
+
+    }
+
+    @Test
+    public void testKeyPressed() throws Exception {
+
+    }
+
+    @Test
+    public void testKeyReleased() throws Exception {
+
+    }
+}
