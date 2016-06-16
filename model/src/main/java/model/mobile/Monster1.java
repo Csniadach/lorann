8ce93c5f29f68f70b6f8fb1b7fa0e1ad53d167a9
Csniadach <cyril.sniadach@viacesi.fr>
2016-06-16 16:02:41 +0200

    monster movements + destroy + death
    state of death is declared, the monsters move randomly, and they
    disapear when hit by a fireball

--- model/src/main/java/model/mobile/Monster1.java
+++ model/src/main/java/model/mobile/Monster1.java
@@ -1,13 +1,22 @@
 package model.mobile;
 
+import contract.IElement;
+import contract.IMonster;
+import contract.MobileOrder;
+
 import java.awt.*;
 
 /**
  * Created by SNIADACH Cyril
  */
-public class Monster1 extends Mobile {
+public class Monster1 extends Mobile implements IMonster {
     public Monster1(Point pos)
     {
         super("monster_1.png", true, pos);
+    }
+
+
+    public MobileOrder getDirection(Point heroPos, IElement[][] tileMap) {
+        return MobileOrder.random();
     }
 }
--- model/src/main/java/model/mobile/Monster4.java
+++ model/src/main/java/model/mobile/Monster4.java
@@ -1,13 +1,22 @@
 package model.mobile;
 
+import contract.IElement;
+import contract.IMonster;
+import contract.MobileOrder;
+
 import java.awt.*;
 
 /**
  * Created by SNIADACH Cyril
  */
-public class Monster4 extends Mobile {
+public class Monster4 extends Mobile implements IMonster {
     public Monster4(Point pos)
     {
         super("monster_4.png", true, pos);
+    }
+
+
+    public MobileOrder getDirection(Point heroPos, IElement[][] tileMap) {
+        return null;
     }
 }
